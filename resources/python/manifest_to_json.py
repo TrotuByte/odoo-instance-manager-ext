a18ec389e2381f82d3521fb613a34dfa44c21323
anescdev <anescdev@gmail.com>
2024-02-08 15:14:19 +0100

    Removed unnecesary python script

--- resources/python/manifest_to_json.py
+++
@@ -1,8 +0,0 @@
-from sys import argv, stderr
-from json import dumps
-if len(argv) != 2:
-    stderr.write("You must pass only the absolute path of the manifest file")
-    exit(1)
-data = open(argv[1])
-print(dumps(eval(data.read())))
-data.close()
